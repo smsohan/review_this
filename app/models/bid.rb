be9c1cf82111dadb2fb492e29c8e8b0368835d2f
smsohan <sohan39@gmail.com>
2012-11-17 18:46:16 -0700

    fixed the comments ordering and the link to comments in the email

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -4,7 +4,7 @@ class Bid < ActiveRecord::Base
   belongs_to :review_request
   belongs_to :bidder, class_name: 'User'
 
-  has_many :comments, dependent: :destroy
+  has_many :comments, dependent: :destroy, order: 'comments.created_at DESC'
 
   validates :bid_amount, presence: true
   validates :bidder_id, presence: true
--- spec/mailers/comment_notifier_spec.rb
+++ spec/mailers/comment_notifier_spec.rb
@@ -7,9 +7,12 @@ describe CommentNotifier do
       requestor = User.create!(name: 'other', email: 'req@email.com', password: 'other1', password_confirmation: 'other1')
       review_request = ReviewRequest.new(title: 'Need resume reviewed'){|rr| rr.requestor = requestor; rr.save!}
 
-      bid = Bid.new
+      bid = Bid.new(bid_amount: 100, bid_message: 'interested')
       bid.review_request = review_request
-      bid.bidder = User.new(email: 'bidder@email.com', name: 'Bidder')
+      bid.bidder = User.create!(email: 'bidder@email.com', name: 'Bidder', password: 'bidder1', password_confirmation: 'bidder1')
+
+      bid.save!
+
       comment = Comment.new{|c| c.bid = bid; c.user = bid.bidder}
 
       email = CommentNotifier.new_comment(comment)
